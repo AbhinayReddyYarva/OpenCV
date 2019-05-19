ffe0a6948abc0c2fb3ee1a6c79ac8683520ce789
Abhinay Reddy Yarva <abhinayreddy98@gmail.com>
2019-05-19 23:20:46 +0530

    To save crop images as data sets to train models

--- VideotoFrames.py
+++ VideotoFrames.py
@@ -2,23 +2,30 @@ import cv2
 import math
 import matplotlib.pyplot as plt
 import pandas as pd
-%matplotlib inline
 from keras.preprocessing import image
 import numpy as np
 
 count = 0
-videoFile = "VID_20190504_082633.mp4"
+videoFile = "TestAnger.mp4"
 cap = cv2.VideoCapture(videoFile)
 frameRate = cap.get(5) #frame rate
-x=1
+print(frameRate)
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 while(cap.isOpened()):
     frameId = cap.get(1) #current frame number
     ret, frame = cap.read()
     if (ret != True):
         break
-    if (frameId % (math.floor(frameRate)/5) == 0):
-        filename ="frame%d.jpg" % count;count+=1
-        cv2.imwrite(filename, frame)
+    if (frameId % (math.floor(frameRate/5)) == 0):
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        #crop = frame
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,0),0)
+            crop_img = frame[y:y+h, x:x+w]
+            #crop = crop_img
+        filename ="DataSet/Test/TestAnger/TestAnger%d.jpg" % count;count+=1
+        cv2.imwrite(filename, crop_img)
 cap.release()
 print ("Done!")
 
